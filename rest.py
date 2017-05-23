27719cb20969dfead5f2281be0f29ebabe83c61b
Bruce Yinhe <bruce.yinhe@gmail.com>
2017-05-23 17:53:10 +0200

    Only count active jobs

--- rest.py
+++ rest.py
@@ -1,9 +1,13 @@
 import json
 from urllib.request import urlopen
+import time
+from datetime import date
 
-url = 'http://erlangcentral.org/wp-json/wp/v2/posts?categories=20'
+
+url = 'http://erlangcentral.org/wp-json/wp/v2/posts?categories=20&per_page=40'
 json_result = json.load(urlopen(url))
 one_post = json_result[2]
+jobs = []
 
 for one_post in json_result:
 
@@ -27,4 +31,9 @@ for one_post in json_result:
     'job_url': job_url,
     'job_location': job_location}
 
-    print (json.dumps(data, indent=4, sort_keys=True))
+    if date.today().isoformat() < job_end_date:
+        print (json.dumps(data, indent=4, sort_keys=True))
+        jobs.append(data)
+
+
+print (len(jobs))
